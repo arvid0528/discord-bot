7b71879b353a0cf91e14baa4eb17c43bf46180fa
Arvid Fredriksson <arvid@famfredriksson.com>
2024-05-20 19:12:26 +0200

    error handling changed again

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import directedcomments as dc
 import reddit_scrape
 import gpt
 import traceback
-
+import datetime
 
 swearwords = ["fuck", "shit", "cunt", "dick", "cum", "piss", "ass", "hell", "cock", "slut", "prick", "bitch", ]
 compliment_comments = []
@@ -24,41 +24,52 @@ class MyClient(discord.Client):
         if message.author == client.user:
             return
         
-        if raw_msg.find("table") >= 0:
-            await message.channel.send("┬─┬")
-
-        for word in swearwords:
-            if raw_msg.find(word) >= 0:
-                if bullshit.increase_bullshit_meter(message.author):
-                    msg = insultgen.generate_insult(message.author)
-                    channel = client.guilds[0].text_channels[0]
-                    await channel.send(msg)
-                break
-        
-        if raw_msg.find("nut") >= 0:
-            f = discord.File(open("elephant.png","rb"))
-            await message.channel.send(file=f)
-
-        if raw_msg.find("among") >= 0:
-            await message.add_reaction("🤨")
-    
-        if raw_msg.find("sad") >= 0:
-            await message.channel.send(quotegen.generate_quote())
-
-        if raw_msg.find("willy") >= 0 or raw_msg.find(client.get_user(1228039945239924756).mention) >= 0: 
-            if not dc.contains_positive_adjective(raw_msg):
-                await message.reply("the fuck you want?")
-                return
-            
         try: 
-            if dc.negations_amount(raw_msg)%2==0:
-                await message.channel.send(gpt.get_compliment(raw_msg, message.author))
-            else:
-                await message.channel.send(gpt.get_insult(raw_msg, message.author))
-        except Exception:
+
+            if raw_msg.find("table") >= 0:
+                await message.channel.send("┬─┬")
+
+            for word in swearwords:
+                if raw_msg.find(word) >= 0:
+                    if bullshit.increase_bullshit_meter(message.author):
+                        msg = insultgen.generate_insult(message.author)
+                        channel = client.guilds[0].text_channels[0]
+                        await channel.send(msg)
+                    break
+            
+            if raw_msg.find("nut") >= 0:
+                f = discord.File(open("elephant.png","rb"))
+                await message.channel.send(file=f)
+
+            if raw_msg.find("among") >= 0:
+                await message.add_reaction("🤨")
+        
+            if raw_msg.find("sad") >= 0:
+                await message.channel.send(quotegen.generate_quote())
+
+            if raw_msg.find("willy") >= 0 or raw_msg.find(client.get_user(1228039945239924756).mention) >= 0: 
+                if not dc.contains_positive_adjective(raw_msg):
+                    await message.reply("the fuck you want?")
+                    return
+
+                if dc.negations_amount(raw_msg)%2==0:
+                    await message.channel.send(gpt.get_compliment(raw_msg, message.author))
+                else:
+                    await message.channel.send(gpt.get_insult(raw_msg, message.author))
+            
+            if raw_msg.find("raise-error") >= 0:
+                raise Exception
+
+        except Exception: 
             print(traceback.format_exc())
+            master = await client.fetch_user(226412002866757642)
+            await message.channel.send(master.mention+" help i made a stinky")
             with open("logfile.txt", "w") as f:
+                f.write(str(datetime.datetime.now())+"\n")
                 f.write(traceback.format_exc())
+            
+            #TODO: make command to have the bot spit out the error message
+            # as a dm maybe? 
 
 intents = discord.Intents.default()
 intents.message_content = True
